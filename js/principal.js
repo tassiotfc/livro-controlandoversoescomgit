765a7013c8a93c663087e190dfbfa0b1ed3f8c3b
Tássio Fernandes Costa <tassiokp206@gmail.com>
2020-03-25 15:05:33 -0300

    Alterando textos do banner

--- js/principal.js
+++ js/principal.js
@@ -1,8 +1,7 @@
-var banners = ["Os melhores do Brasil!", "Qualidade e preço baixo!"];
+var banners = ["Do lixo ao luxo!", "Reaproveitar é aproveitar!"];
 var bannerAtual = 0;
 function trocaBanner() {
-    bannerAtual = (bannerAtual + 1) % 2;
-    document.querySelector('h2#mensagem').textContent =
-        banners[bannerAtual];
+  bannerAtual = (bannerAtual + 1) % 2;
+  document.querySelector("h2#mensagem").textContent = banners[bannerAtual];
 }
 setInterval(trocaBanner, 1000);
